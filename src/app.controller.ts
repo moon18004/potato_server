6752cbdcd0eeb153dca76ab7949f0ee291a5ed29
Moon Moon <purnsup90@gmail.com>
2023-12-13 19:03:05 -0700

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHome(){
+    return 'Hello World';
+  }
+  @Get('post')
+  getPost(){
+    return 'Post Page';
+  }
+  @Get('user')
+  getUser(){
+    return 'User';
+  }
+}
