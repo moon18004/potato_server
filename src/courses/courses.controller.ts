c4c3a5969bab1837f86e374d0c95e1b6e3997bd4
Moon Moon <purnsup90@gmail.com>
2024-01-24 21:02:03 -0700

    asdf

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,8 +1,11 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 import { UsersModel } from 'src/users/entities/users.entity';
+<<<<<<< HEAD
 import { CreateCourseDto } from './dto/create-course.dto';
 import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
+=======
+>>>>>>> 0a0c982 (asdf)
 
 @Controller('courses')
 export class CoursesController {
@@ -14,8 +17,18 @@ export class CoursesController {
   }
 
   @Post()
+<<<<<<< HEAD
   @UseGuards(AccessTokenGuard)
   postCourse(@Body() user: UsersModel, @Body() body: CreateCourseDto) {
+=======
+  postCourse(
+    @Body('author') author: UsersModel,
+    @Body('class_name') class_name: string,
+    @Body('content') content: string,
+    @Body('subject') subject: string,
+    @Body('class_code') class_code: string
+  ) {
+>>>>>>> 0a0c982 (asdf)
     console.log('coutse post');
     return this.coursesService.createCourse(user.id, body);
   }
--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -3,7 +3,10 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CoursesModel } from './entities/courses.entity';
 import { Repository } from 'typeorm';
 import { UsersModel } from 'src/users/entities/users.entity';
+<<<<<<< HEAD
 import { CreateCourseDto } from './dto/create-course.dto';
+=======
+>>>>>>> 0a0c982 (asdf)
 
 @Injectable()
 export class CoursesService {
@@ -14,7 +17,18 @@ export class CoursesService {
   async getCourses() {
     return await this.coursesRepository.find();
   }
+<<<<<<< HEAD
   async createCourse(author_id: number, courseDto: CreateCourseDto) {
+=======
+  async createCourse(
+    author: UsersModel,
+    class_name: string,
+    content: string,
+    subject: string,
+    class_code: string
+  ) {
+    console.log(subject);
+>>>>>>> 0a0c982 (asdf)
     const course = this.coursesRepository.create({
       author: {
         id: author_id
