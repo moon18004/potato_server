80655b536761d279e4d2980fda1366cd004ada36
Moon Moon <purnsup90@gmail.com>
2024-02-17 03:09:53 -0700

    community

---
+++ src/posts/const/category.const.ts
@@ -0,0 +1,12 @@
+export enum CategoryEnum {
+  // 일반
+  QUESTION = 'QUESTION',
+  // 인기
+  CHITCHAT = 'CHITCHAT',
+  // 공지
+  SOCIAL = 'SOCIAL',
+
+  TIP = 'TIP',
+
+  ETC = 'ETC'
+}
--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -5,7 +5,7 @@ import { PickType } from '@nestjs/mapped-types';
 // Pick, Omit, Partial => Type을 반환
 // PickType, OmitType, PartialType => 값을 반환
 
-export class CreatePostDto extends PickType(PostsModel, ['title', 'content']) {
+export class CreatePostDto extends PickType(PostsModel, ['title', 'content', 'category']) {
   // @IsString({
   //   message: 'Titile should be string type.'
   // })
